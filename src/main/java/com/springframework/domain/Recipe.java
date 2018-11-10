8f0c8c886aa573de991bdba566c2557f30b463d7
Jan Vachuda <vachudj@gmail.com>
2018-11-10 17:39:36 +0100

    ManyToOne/OneToMany relationships introduced to Recipe

--- src/main/java/com/springframework/domain/Recipe.java
+++ src/main/java/com/springframework/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.springframework.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -20,6 +21,9 @@ public class Recipe {
 
     @Lob
     private Byte[] image;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     public Long getId() {
         return id;
@@ -102,5 +106,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
