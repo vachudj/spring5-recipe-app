2a3e07682b067a08a8e1f9897ce9b95eae5ec373
Jan Vachuda <vachudj@gmail.com>
2018-11-18 18:55:40 +0100

    Method sql introduction.

--- src/main/java/com/springframework/controllers/IndexController.java
+++ src/main/java/com/springframework/controllers/IndexController.java
@@ -1,13 +1,40 @@
 package com.springframework.controllers;
 
+import com.springframework.domain.Category;
+import com.springframework.domain.UnitOfMeasure;
+import com.springframework.repositories.CategoryRepository;
+import com.springframework.repositories.UnitOfMeasureRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Controller
 public class IndexController {
 
+    private static final Logger logger = getLogger(IndexController.class);
+
+    private final CategoryRepository categoryRepository;
+
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        logger.info("Cat id is: " + categoryOptional.get().getId());
+        logger.info("UOM id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
